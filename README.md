60dd2befa5f6f59aa82fa01e93e3bfd219f0a856
Ju Chanhyoung <ju031230321@gmail.com>
2024-03-21 14:17:35 +0900

    Update examples and README.

--- README.md
+++ README.md
@@ -12,6 +12,9 @@ And then you can draw them!!
 ## Example
 Full examples is in [main.cc](./src/main.cc).
 ```C++
+// Disable buffer for fast printing.
+ConsoleCoordinate::FastIO();
+
 ConsoleCoordinate cc(10);
 // Define shape s.
 cc.AddShpae(s);
@@ -20,13 +23,18 @@ int64_t sleep_time = 100;
 // x, y and z are (radian) angle for lotation.
 ConsoleCoordinate::FloatType x(0.03), y(0.03), z(0.03);
 while (1) {
+  cc.SyncConsoleSize();
+
   cc.Clear();
+  ConsoleCoordinate::ClearScreen();
+
   cc.Draw();
-  std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
   // Lotate every shape around ? axis
   cc.LotateEveryShapeAroundXAxis(x);
   cc.LotateEveryShapeAroundYAxis(y);
   cc.LotateEveryShapeAroundZAxis(z);
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
 }
 ```
 
--- src/main.cc
+++ src/main.cc
@@ -6,20 +6,36 @@
 
 using namespace lotate_polyhedron;
 
-void LotateCube(void);
+void AddCubeToCoordinate(ConsoleCoordinate& cc);
 
-void LotatePyramid(void);
-
-void LotateCubeAndPyramid(void);
+void AddPyramidToCoordinate(ConsoleCoordinate& cc);
 
 int main() {
-  // LotatePyramid();
-  // LotateCube();
-  LotateCubeAndPyramid();
+  ConsoleCoordinate::FastIO();
+
+  ConsoleCoordinate cc(10);
+  AddCubeToCoordinate(cc);
+  AddPyramidToCoordinate(cc);
+
+  int64_t sleep_time = 100;
+  ConsoleCoordinate::FloatType x(0.03), y(0.03), z(0.03);
+  while (1) {
+    cc.SyncConsoleSize();
+
+    cc.Clear();
+    ConsoleCoordinate::ClearScreen();
+
+    cc.Draw();
+    cc.LotateEveryShapeAroundXAxis(x);
+    cc.LotateEveryShapeAroundYAxis(y);
+    cc.LotateEveryShapeAroundZAxis(z);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+  }
   return 0;
 }
 
-void LotateCube(void) {
+void AddCubeToCoordinate(ConsoleCoordinate& cc) {
   std::vector<ConsoleCoordinate::DotType> vbo = {
       // Bottom four dots
       ConsoleCoordinate::DotType(1, 1, -1),
@@ -50,23 +66,10 @@ void LotateCube(void) {
       {3, 7},
   };
   ConsoleCoordinate::ShapeType s(vbo, ebo);
-
-  ConsoleCoordinate cc(10);
   cc.AddShpae(s);
-  int64_t sleep_time = 100;
-  ConsoleCoordinate::FloatType x(0.03), y(0.03), z(0.03);
-  while (1) {
-    cc.SyncConsoleSize();
-    cc.Clear();
-    cc.Draw();
-    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
-    cc.LotateEveryShapeAroundXAxis(x);
-    cc.LotateEveryShapeAroundYAxis(y);
-    cc.LotateEveryShapeAroundZAxis(z);
-  }
 }
 
-void LotatePyramid(void) {
+void AddPyramidToCoordinate(ConsoleCoordinate& cc) {
   std::vector<ConsoleCoordinate::DotType> vbo = {
       // Bottom four dots
       ConsoleCoordinate::DotType(1, 1, -1),
@@ -90,93 +93,5 @@ void LotatePyramid(void) {
 
   };
   ConsoleCoordinate::ShapeType s(vbo, ebo);
-
-  ConsoleCoordinate cc(10);
   cc.AddShpae(s);
-  int64_t sleep_time = 100;
-  ConsoleCoordinate::FloatType x(0.03), y(0.03), z(0.03);
-  while (1) {
-    cc.SyncConsoleSize();
-    cc.Clear();
-    cc.Draw();
-    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
-    cc.LotateEveryShapeAroundXAxis(x);
-    cc.LotateEveryShapeAroundYAxis(y);
-    cc.LotateEveryShapeAroundZAxis(z);
-  }
-}
-
-void LotateCubeAndPyramid(void) {
-  ConsoleCoordinate cc(10);
-  {
-    std::vector<ConsoleCoordinate::DotType> vbo = {
-        // Bottom four dots
-        ConsoleCoordinate::DotType(1, 1, -1),
-        ConsoleCoordinate::DotType(-1, 1, -1),
-        ConsoleCoordinate::DotType(-1, -1, -1),
-        ConsoleCoordinate::DotType(1, -1, -1),
-        // Top dot
-        ConsoleCoordinate::DotType(0, 0, 1),
-    };
-    std::vector<ConsoleCoordinate::LineIndicatorType> ebo = {
-        // Bottom Lines
-        {0, 1},
-        {1, 2},
-        {2, 3},
-        {3, 0},
-        // Lines To Top
-        {0, 4},
-        {1, 4},
-        {2, 4},
-        {3, 4},
-
-    };
-    ConsoleCoordinate::ShapeType s(vbo, ebo);
-    cc.AddShpae(std::move(s));
-  }
-  {
-    std::vector<ConsoleCoordinate::DotType> vbo = {
-        // Bottom four dots
-        ConsoleCoordinate::DotType(1, 1, -1),
-        ConsoleCoordinate::DotType(-1, 1, -1),
-        ConsoleCoordinate::DotType(-1, -1, -1),
-        ConsoleCoordinate::DotType(1, -1, -1),
-        // Top four dots
-        ConsoleCoordinate::DotType(1, 1, 1),
-        ConsoleCoordinate::DotType(-1, 1, 1),
-        ConsoleCoordinate::DotType(-1, -1, 1),
-        ConsoleCoordinate::DotType(1, -1, 1),
-    };
-    std::vector<ConsoleCoordinate::LineIndicatorType> ebo = {
-        // Bottom Lines
-        {0, 1},
-        {1, 2},
-        {2, 3},
-        {3, 0},
-        // Top Lines
-        {4, 5},
-        {5, 6},
-        {6, 7},
-        {7, 4},
-        // Pillar Lines
-        {0, 4},
-        {1, 5},
-        {2, 6},
-        {3, 7},
-    };
-    ConsoleCoordinate::ShapeType s(vbo, ebo);
-    cc.AddShpae(std::move(s));
-  }
-
-  int64_t sleep_time = 100;
-  ConsoleCoordinate::FloatType x(0.03), y(0.03), z(0.03);
-  while (1) {
-    cc.SyncConsoleSize();
-    cc.Clear();
-    cc.Draw();
-    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
-    cc.LotateEveryShapeAroundXAxis(x);
-    cc.LotateEveryShapeAroundYAxis(y);
-    cc.LotateEveryShapeAroundZAxis(z);
-  }
 }
